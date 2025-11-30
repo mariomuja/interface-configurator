4c582d4596043df5cb5042effaf9d24af9c8cfff
mariomuja <mario.muja@gmx.de>
2025-11-30 12:44:55 +0100

    fix: remove obsolete test files referencing deprecated MessageBox architecture

--- services/obj/Release/net8.0/services.AssemblyInfo.cs
+++ services/obj/Release/net8.0/services.AssemblyInfo.cs
@@ -11,9 +11,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("services")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99fbbdecf4f335b50367fda3d22335b676f721b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe51f8a61dd3b2b33c2e6d3c996fcab2bd585f54")]
 [assembly: System.Reflection.AssemblyProductAttribute("services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
